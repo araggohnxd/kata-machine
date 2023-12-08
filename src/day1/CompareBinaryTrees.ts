3a728f32afd6ec4b928a45b453f93951534a0f75
Matheus Oliveira <31555284+araggohnxd@users.noreply.github.com>
2023-12-08 16:38:24 -0300

    day(1): compare binary trees

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,26 @@
-export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+export default function compare(
+    a: BinaryNode<number> | null,
+    b: BinaryNode<number> | null,
+): boolean {
+    // 1. structural check
+    if (!a && !b) {
+        return true;
+    }
 
+    // 2. structural check
+    if (!a || !b) {
+        return false;
+    }
+
+    // 3. value check
+    if (a.value !== b.value) {
+        return false;
+    }
+
+    // 2. and 3. can be condensed in Typescript:
+    // if (a?.value !== b?.value) {
+    //     return false
+    // }
+
+    return compare(a.left, b.left) && compare(a.left, b.left);
 }
