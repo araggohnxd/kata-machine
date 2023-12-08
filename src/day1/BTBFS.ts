42e7fb1db067c5fa65ceea41454bf6f47adbbc65
Matheus Oliveira <31555284+araggohnxd@users.noreply.github.com>
2023-12-08 15:17:39 -0300

    day(1): breadth first search

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,23 @@
-export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+import Queue from "./Queue";
 
+export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const q = new Queue<BinaryNode<number> | null>();
+
+    q.enqueue(head);
+    while (q.length) {
+        const curr = q.deque() as BinaryNode<number> | null | undefined;
+
+        if (!curr) {
+            continue;
+        }
+
+        if (curr.value === needle) {
+            return true;
+        }
+
+        q.enqueue(curr.left);
+        q.enqueue(curr.right);
+    }
+
+    return false;
 }
